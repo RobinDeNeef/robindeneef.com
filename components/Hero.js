aeea86e76aa5b7d30d85b3d39efd3665850153be
Robin De Neef <deneefrobin@gmail.com>
2023-02-24 23:56:33 +0100

    Update packages

--- components/Hero.js
+++ components/Hero.js
@@ -1,60 +1,41 @@
-import React, { useState } from "react";
-import Link from "next/link";
-import profile from "../public/profile.png";
+import React, { useState } from 'react'
+import Link from 'next/link'
+import profile from '../public/profile.png'
 
-import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
+import { RoughNotation, RoughNotationGroup } from 'react-rough-notation'
 
 const Hero = () => {
-  const [invertColor, setInvertColor] = useState(false);
+  const [invertColor, setInvertColor] = useState(false)
 
   const invertColorFunction = () => {
-    setTimeout(() => setInvertColor(!invertColor), 3500);
-  };
+    setTimeout(() => setInvertColor(!invertColor), 3500)
+  }
 
   return (
-    <div className="container px-4 mx-auto lg:max-w-4xl">
-      <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left lg:mt-6">
-        <div className="lg:px-4 lg:mt-8 ">
-          <h1 className="text-4xl font-bold text-gray-900 lg:text-5xl dark:text-white">
+    <div className="container mx-auto px-4 lg:max-w-4xl">
+      <div className="item-center flex flex-col-reverse text-center lg:-mx-4 lg:mt-6 lg:flex lg:flex-row lg:space-x-5 lg:text-left">
+        <div className="lg:mt-8 lg:px-4 ">
+          <h1 className="text-4xl font-bold text-gray-900 dark:text-white lg:text-5xl">
             Welcome! 👋
           </h1>
-          <div className="mt-4 text-gray-800 dark:text-white mx-2 lg:mx-0">
+          <div className="mx-2 mt-4 text-gray-800 dark:text-white lg:mx-0">
             <RoughNotationGroup show={true}>
               <p className="mb-4 text-xl">
-                My name is{" "}
-                <RoughNotation
-                  type="underline"
-                  padding={[-2, 0, 0, 0]}
-                  order="1"
-                >
+                My name is{' '}
+                <RoughNotation type="underline" padding={[-2, 0, 0, 0]} order="1">
                   Robin
                 </RoughNotation>
-                . By day, I’m a{" "}
-                <RoughNotation
-                  type="underline"
-                  padding={[-2, 0, 0, 0]}
-                  order="2"
-                  multiline
-                >
-                  <RoughNotation
-                    type="highlight"
-                    color="orange"
-                    order="4"
-                    multiline
-                  >
-                    <span className={invertColor ? "text-black" : undefined}>
-                      Fullstack Developer at ML6
+                . By day, I’m a{' '}
+                <RoughNotation type="underline" padding={[-2, 0, 0, 0]} order="2" multiline>
+                  <RoughNotation type="highlight" color="orange" order="4" multiline>
+                    <span className={invertColor ? 'text-black' : undefined}>
+                      freelance software developer
                     </span>
                   </RoughNotation>
                 </RoughNotation>
-                . By night, I like to tinker around with different technologies
-                and build little projects of my own at{" "}
-                <RoughNotation
-                  type="underline"
-                  padding={[-2, 0, 0, 0]}
-                  order="3"
-                  multiline
-                >
+                . By night, I like to tinker around with different technologies and build little
+                projects of my own at{' '}
+                <RoughNotation type="underline" padding={[-2, 0, 0, 0]} order="3" multiline>
                   <RoughNotation
                     type="highlight"
                     color="yellow"
@@ -62,14 +43,8 @@ const Hero = () => {
                     getAnnotationObject={invertColorFunction}
                     multiline
                   >
-                    <Link
-                      href="https://sobrinostudios.com"
-                      passHref={true}
-                      component="a"
-                    >
-                      <a className={invertColor ? "text-black" : undefined}>
-                        Sobrino Studios.
-                      </a>
+                    <Link href="https://sobrinostudios.com" passHref={true} component="a">
+                      <a className={invertColor ? 'text-black' : undefined}>Sobrino Studios.</a>
                     </Link>
                   </RoughNotation>
                 </RoughNotation>
@@ -77,7 +52,7 @@ const Hero = () => {
             </RoughNotationGroup>
           </div>
         </div>
-        <div className="flex lg:flex-shrink-0 justify-center lg:mt-5 lg:px-4 my-10 lg:my-1">
+        <div className="my-10 flex justify-center lg:my-1 lg:mt-5 lg:flex-shrink-0 lg:px-4">
           <img
             src="/profile.png"
             alt="Profile"
@@ -89,7 +64,7 @@ const Hero = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
---
+++ utils/generateSitemap.js
@@ -0,0 +1,71 @@
+
+const fs = require('fs')
+const globby = require('globby')
+const matter = require('gray-matter')
+const prettier = require('prettier')
+
+const SITE_URL="https://robindeneef.com"
+
+;(async () => {
+  const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
+  const pages = await globby([
+    'pages/**/*.js',
+    'pages/**/*.tsx',
+    '_makerlog/**/*.mdx',
+    '_makerlog/**/*.md',
+    '_posts/**/*.mdx',
+    '_posts/**/*.md',
+    'public/tags/**/*.xml',
+    '!pages/_*.js',
+    '!pages/_*.tsx',
+    '!pages/**/[slug].js',
+    '!pages/api',
+  ])
+  const sitemap = `
+        <?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            ${pages
+              .map((page) => {
+                // Exclude drafts from the sitemap
+                if (page.search('.md') >= 1 && fs.existsSync(page)) {
+                  const source = fs.readFileSync(page, 'utf8')
+                  const fm = matter(source)
+                  if (fm.data.draft) {
+                    return
+                  }
+                  if (fm.data.canonicalUrl) {
+                    return
+                  }
+                }
+                const path = page
+                  .replace('pages/', '/')
+                  .replace('_makerlog/', '/makerlog/')
+                  .replace('_posts/', '/blog/')
+                  .replace('public/', '/')
+                  .replace('.js', '')
+                  .replace('.tsx', '')
+                  .replace('.mdx', '')
+                  .replace('.md', '')
+                  .replace('/feed.xml', '')
+                const route = path.match(/index/) ? path.replace('/index', '') : path
+                if (page.search('pages/404.') > -1 || page.search(`pages/blog/[...slug].`) > -1) {
+                  return
+                }
+                return `
+                        <url>
+                            <loc>${SITE_URL}${route}</loc>
+                        </url>
+                    `
+              })
+              .join('')}
+        </urlset>
+    `
+
+  const formatted = prettier.format(sitemap, {
+    ...prettierConfig,
+    parser: 'html',
+  })
+
+  // eslint-disable-next-line no-sync
+  fs.writeFileSync('public/sitemap.xml', formatted)
+})()
