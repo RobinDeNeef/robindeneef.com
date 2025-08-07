f7581332e464e1db7e2cd82cf314e5f6a45b117d
Robin De Neef <deneefrobin@gmail.com>
2025-08-07 15:06:45 +0200

    wip: Correct Folder Structure

--- current/src/utils/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    // REQUIRED
-    siteName: "Robin De Neef",
-    // REQUIRED: a short description of your app for SEO tags (can be overwritten)
-    siteDescription:
-        "Data/AI Consultant. Founder of Sobrino Studios. Building Data and AI Products for Startups and Enterprises. Currently Data/AI Lead at JustRussel",
-};
-
-export default config;
